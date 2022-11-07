968c64ed5da9628c7e386b2dce8fa7b49b284b9e
chinchin <skykim10908@naver.com>
2022-11-07 16:51:19 +0900

    flow Job, decider !!

--- src/main/java/io/springbatch/springbatch/JobExcutionDeciderConfiguration.java
+++ src/main/java/io/springbatch/springbatch/JobExcutionDeciderConfiguration.java
@@ -7,15 +7,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @RequiredArgsConstructor
-//@Configuration
-public class CustomExitStatusConfiguration
+public class JobExcutionDeciderConfiguration
 {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -23,18 +24,23 @@ public class CustomExitStatusConfiguration
     @Bean
     public Job batchJob()
     {
-        return jobBuilderFactory.get("customJob")
-                .start( Step1() )
-                    .on("FAILED")
-                    .to( Step2() )
-                    .on("PASS")
-                    .stop()
+        return jobBuilderFactory.get("deciderJob")
+                .start( Step() )
+                .next( decider() )
+                .from( decider() ).on("ODD").to( OddStep() )
+                .from( decider() ).on("EVEN").to( EvenStep())
                 .end()
                 .build();
     }
 
     @Bean
-    public Step Step1()
+    public JobExecutionDecider decider()
+    {
+        return new CustomDecider();
+    }
+
+    @Bean
+    public Step Step()
     {
         return stepBuilderFactory
                 .get("Step1")
@@ -43,27 +49,43 @@ public class CustomExitStatusConfiguration
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception
                     {
-                        System.out.println(" step1 has executed ");
-                        stepContribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+                        System.out.println("this is the start tasklet");
 
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
-    public Step Step2()
+    public Step EvenStep()
     {
         return stepBuilderFactory
-                .get("Step2")
+                .get("EvenStep")
                 .tasklet(new Tasklet()
                 {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception
                     {
-                        System.out.println(" >> step2 was executed ");
+                        System.out.println(" >> EvenStep was executed ");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
+
+    public Step OddStep()
+    {
+        return stepBuilderFactory
+                .get("OddStep")
+                .tasklet(new Tasklet()
+                {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception
+                    {
+                        System.out.println(" >> OddStep was executed ");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
 }
