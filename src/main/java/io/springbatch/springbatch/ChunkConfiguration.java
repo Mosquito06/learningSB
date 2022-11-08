adf3f6f6009a44cb8352d23ce55cf4e857407453
chinchin <skykim10908@naver.com>
2022-11-08 14:21:56 +0900

    Chunk basic !

--- src/main/java/io/springbatch/springbatch/ChunkConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ChunkConfiguration.java
@@ -8,9 +8,15 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,8 +28,7 @@ public class ChunkConfiguration
     @Bean
     public Job batchJob()
     {
-        return jobBuilderFactory
-                .get("chunkJob")
+        return jobBuilderFactory.get("chunkJob")
                 .start(Step1())
                 .next( Step2() )
                 .build();
@@ -34,17 +39,28 @@ public class ChunkConfiguration
     {
         return stepBuilderFactory
                 .get("Step1")
-                .tasklet(new Tasklet()
-                {
+                .<String, String>chunk(5)
+                .reader( new ListItemReader<>(Arrays.asList("item1", "item2", "ite3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
                     @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception
+                    public String process(String s) throws Exception
                     {
-                        System.out.println(" step1 has executed ");
-                        return RepeatStatus.FINISHED;
+                        Thread.sleep(300);
+                        System.out.println("item = " + s);
+                        return "my" + s;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> list) throws Exception
+                    {
+                        Thread.sleep(300);
+                        System.out.println("items = " + list);
                     }
                 })
                 .build();
     }
+
     public Step Step2()
     {
         return stepBuilderFactory
